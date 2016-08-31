e30ba429b758f5546bd77643d1c2999fcd7df783
nurcahyo <nurcahyopujo@gmail.com>
2016-08-31 16:45:30 +0700

    create components note

--- components/ListNotes.js
+++ components/ListNotes.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
+import Note from './Note.js';
+
 class ListNotes extends React.Component {
   render() {
     return (
       <div>
-        <div className="well">
-          <h4>Note 1</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-        </div>
-        <div className="well">
-          <h4>Note 1</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-        </div>
-        <div className="well">
-          <h4>Note 1</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-        </div>
+        <Note />
+        <Note />
+        <Note />
       </div>
     );
   }
---
+++ components/Note.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Note extends React.Component {
+  render() {
+    return (
+      <div className="well">
+        <h4>Note 1</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+      </div>
+    );
+  }
+}
+
+export default Note;
